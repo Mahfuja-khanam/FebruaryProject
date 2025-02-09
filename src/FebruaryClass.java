6d117a7f6f52b5dac1af99ef255d90095219bd29
Mahfuja <mahfuja.khanam04@gmail.com>
2025-02-09 10:17:13 -0500

    Pushed commit

---
+++ src/FebruaryClass.java
@@ -0,0 +1,18 @@
+public class FebruaryClass{
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 10;
+        int c = a+b;
+        int multi = a*b;
+        double div = (double) a /b;
+
+        System.out.println("sum of a+b "+c);
+        System.out.println("subtraction of a-b " +(a-b));
+        System.out.println("multiplication = " + multi);
+        System.out.println( "div = "+ div);
+
+    }
+
+
+}
